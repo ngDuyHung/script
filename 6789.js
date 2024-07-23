eacf62e33b05613b3024987518989a30e521a53b
Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2024-07-23 15:24:42 +0700

    Update 6789.js
    trhf

--- 6789.js
+++ 6789.js
@@ -1,5 +1,11 @@
-fdsgfdsgsdfsg
-duyhung
-dfgsfds
-<P>fgđfgadfg</p>
-ds
+// 6789.js
+console.log('JavaScript file is successfully loaded!');
+
+document.addEventListener('DOMContentLoaded', function() {
+    var testElement = document.getElementById('test-element');
+    if (testElement) {
+        testElement.innerText = 'JavaScript has successfully modified this text!';
+    } else {
+        console.log('Element with id "test-element" not found.');
+    }
+});
