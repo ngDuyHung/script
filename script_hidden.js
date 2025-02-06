7441257fce820a224b0cacf25d5ad88d99eddb36
Nguyễn Duy Hùng <149739374+ngDuyHung@users.noreply.github.com>
2025-02-06 14:08:21 +0700

    Update script_hidden.js
    gdsfgs

--- script_hidden.js
+++ script_hidden.js
@@ -6,7 +6,7 @@ var _0x15d966=_0x4e48;(function(_0x41f1c0,_0x23a6d0){var _0x21ab4a=_0x4e48,_0xca
    
     
 
-    document.addEventListener("DOMContentLoaded", function () {
+     document.addEventListener("DOMContentLoaded", function () {
         document.querySelectorAll("table.table-module tbody").forEach(tbody => {
             let anyRowsVisible = false;
 
@@ -24,7 +24,7 @@ var _0x15d966=_0x4e48;(function(_0x41f1c0,_0x23a6d0){var _0x21ab4a=_0x4e48,_0xca
                 const newRowHTML = `
                     <tr class="notification-row">
                         <td colspan="10">
-                            <div class="text-xs-nowrap text-center font-weight-bold text-danger doithe1svn">Vui lòng đăng nhập để xem chiết khấu</div>
+                            <div class="text-xs-nowrap text-center font-weight-bold text-danger doithecao24hvn">Vui lòng đăng nhập để xem chiết khấu</div>
                         </td>
                     </tr>
                 `;
@@ -35,7 +35,6 @@ var _0x15d966=_0x4e48;(function(_0x41f1c0,_0x23a6d0){var _0x21ab4a=_0x4e48,_0xca
             }
         });
     });
-
 
 
 
